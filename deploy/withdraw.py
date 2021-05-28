caad5318fc97fa72da79e1bb305d7ca803b56358
Dominik Ernst - acria.network <79003106+acria-dominik@users.noreply.github.com>
2021-05-28 16:44:20 +0200

    update

---
+++ deploy/withdraw.py
@@ -0,0 +1,26 @@
+import json
+from web3 import Web3, HTTPProvider, IPCProvider
+from web3.middleware import geth_poa_middleware
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+load_dotenv(join(dirname(__file__), '.env'))
+
+if(os.environ.get("WEB3_USE_IPC") == False):
+    web3 = Web3(HTTPProvider(os.environ.get("WEB3_HTTP_PROVIDER_URL")))
+else:
+    web3 = Web3(IPCProvider(os.environ.get("WEB3_IPC_PROVIDER_URL")))
+
+if(os.environ.get("WEB3_MIDDLEWARE_ONION_INJECT")):    
+    web3.middleware_onion.inject(geth_poa_middleware, layer=0)
+
+web3.eth.defaultAccount = web3.eth.accounts[1]
+
+with open('../build/contracts/AcriaNode.json') as file:
+    contract_json = json.load(file)
+    contract_abi = contract_json['abi']
+
+AcriaNode = web3.eth.contract(address=os.environ.get("ACRIA_NODE_ADDRESS"), abi=contract_abi)
+transaction = AcriaNode.functions.payout_stakes(web3.eth.accounts[1]).transact()
+print(transaction)
